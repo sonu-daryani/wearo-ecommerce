a48840d72f579db7734bba10f85d798c970ca3d8
sonu-daryani <sldaryani007@gmail.com>
2026-04-10 12:33:37 +0530

    Refactor store initialization in Providers component to use useRef for improved performance and prevent unnecessary re-creation of the store.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore } from "../lib/store";
 import { PersistGate } from "redux-persist/integration/react";
@@ -14,7 +14,11 @@ type Props = {
 };
 
 const Providers = ({ children }: Props) => {
-  const { store, persistor } = makeStore();
+  const storeRef = useRef<ReturnType<typeof makeStore>>();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+  const { store, persistor } = storeRef.current;
 
   return (
     <AuthSessionProvider>
